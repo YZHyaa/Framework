d215980a4bf0a41888b493c4a698099c7a0ba6f2
A minor <46704878+YZHyaa@users.noreply.github.com>
2021-01-19 01:33:13 +0800

    Add files via upload

---
+++ myrpc-v2/src/test/java/com/xupt/yzh/provider/TestServiceImpl.java
@@ -0,0 +1,19 @@
+package com.xupt.yzh.provider;
+
+import com.xupt.yzh.api.TestService;
+import com.xupt.yzh.rpc.framework.annotation.RpcService;
+
+import java.util.Random;
+
+@RpcService(TestService.class)
+public class TestServiceImpl implements TestService {
+
+    @Override
+    public String test(String name) {
+        System.out.println("new requst coming..." + name);
+
+        Random random = new Random();
+        String json = "{\"name\":" + "\"" + name + "\"" + ", \"age\":" + random.nextInt(40) + "}";
+        return json;
+    }
+}

837b172c860a0862b8bf3127c184a1491b243e05
A minor <46704878+YZHyaa@users.noreply.github.com>
2021-01-19 01:31:17 +0800

    Add files via upload

---
+++ myrpc-v1/src/main/java/com/xupt/yzh/rpc/framework/consumer/RpcNetTransport.java
@@ -0,0 +1,66 @@
+package com.xupt.yzh.rpc.framework.consumer;
+
+
+import com.xupt.yzh.rpc.framework.protocol.RpcRequest;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * 实现网络调用
+ */
+public class RpcNetTransport {
+
+    private String host;
+    private int port;
+
+    public RpcNetTransport(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public Object send(RpcRequest request) {
+        Socket socket = null;
+        Object result = null;
+        ObjectInputStream inputStream = null;
+        ObjectOutputStream outputStream = null;
+
+        try {
+            // 将调用的服务的具体信息通过网络写出
+            socket = new Socket(host, port);
+
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+            // writeObject实际上是一种序列化
+            outputStream.writeObject(request);
+            outputStream.flush();
+
+            // 阻塞等待Provider的返回结果
+            inputStream = new ObjectInputStream(socket.getInputStream());
+            result = inputStream.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
---
+++ myrpc-v1/src/main/java/com/xupt/yzh/rpc/framework/provider/RpcServer.java
@@ -0,0 +1,41 @@
+package com.xupt.yzh.rpc.framework.provider;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 代理对象，封装了具体对Provider的调用逻辑
+ */
+public class RpcServer {
+    // 用线程池实现给所有连接都分配一个线程
+    ExecutorService executorServic = Executors.newCachedThreadPool();
+
+    // 发布服务
+    public void publisher(Object service, int port) {
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                // 每一个socket，交给一个processorHandler去处理
+                executorServic.execute(new ProcessorHandler(socket, service));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocket != null){
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+}
